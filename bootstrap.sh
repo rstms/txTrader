6cfb487a1d6b3bc9b4532618f5a5e61f715571b4
Matt Krueger <mkrueger@rstms.net>
2015-04-29 01:22:39 -0500

    added package dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 aptitude -y update
-aptitude -y install make python-twisted-web python-egenix-mx-base-dev daemontools-run ucspi-tcp
+aptitude -y install make python-twisted-web python-egenix-mx-base-dev python-egenix-mxdatetime daemontools-run ucspi-tcp
 curl --location -o- https://github.com/rstms/txTrader/tarball/master | tar zxfv -
 cd rstms-txTrader-*
 make config
