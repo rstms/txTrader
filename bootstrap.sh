ece2be1fb0fa7d9bea971aed9780ba591e8f3171
Matt Krueger <mkrueger@rstms.net>
2015-04-29 01:39:25 -0500

    added IbPy to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 aptitude -y update
 aptitude -y install make python-twisted-web python-egenix-mx-base-dev python-egenix-mxdatetime daemontools-run ucspi-tcp
+curl --location https://github.com/rstms/IbPy/tarball/master | tar zxfv -
+cd rstms-IbPy-*
+python setup.py install
+cd
 curl --location -o- https://github.com/rstms/txTrader/tarball/master | tar zxfv -
 cd rstms-txTrader-*
 make config
