9d8bbe7a14ebfbe02a57d7944533c3e5ca486e2c
Matt Krueger <mkrueger@rstms.net>
2015-09-09 16:13:45 -0500

    added ibpy version, changed version string to include v prefix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #aptitude -y update
 #aptitude -y install make python-twisted-web python-egenix-mx-base-dev python-egenix-mxdatetime daemontools-run ucspi-tcp
-curl --location https://github.com/rstms/IbPy/tarball/master | tar zxfv -
+curl --location https://github.com/rstms/IbPy/tarball/$IBPY_VERSION | tar zxfv -
 mv rstms-IbPy-* IbPy
 cd IbPy
 python setup.py install
 cd
-curl --location -o- https://github.com/rstms/txTrader/tarball/v$TXTRADER_VERSION | tar zxfv -
+curl --location -o- https://github.com/rstms/txTrader/tarball/$TXTRADER_VERSION | tar zxfv -
 mv rstms-txTrader-* txTrader
 cd txTrader
 make config
