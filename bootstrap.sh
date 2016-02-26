7496dbd3331842fbbfcae4ee9370375b21043baf
Matt Krueger <mkrueger@rstms.net>
2016-02-26 16:03:20 -0600

    added default version 'master' if environment not set in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+[ -n "$IBPY_VERSION" ] || IBPY_VERSION=master
 curl --location https://github.com/rstms/IbPy/tarball/$IBPY_VERSION | tar zxfv -
 mv rstms-IbPy-* IbPy
 cd IbPy
 python setup.py sdist
 cd
+[ -n "$TXTRADER_VERSION" ] || TXTRADER_VERSION=master
 curl --location -o- https://github.com/rstms/txTrader/tarball/$TXTRADER_VERSION | tar zxfv -
 mv rstms-txTrader-* txTrader
 cd txTrader
-make install
+sudo make install
